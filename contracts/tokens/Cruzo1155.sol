2b83d9fcf57828442535c40171d46364d04850cd
devstar1992 <75045062+devstar1992@users.noreply.github.com>
2022-05-26 01:02:58 +0900

    Initial commit

---
+++ contracts/tokens/Cruzo1155.sol
@@ -0,0 +1,141 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "./ERC1155URI.sol";
+
+contract Cruzo1155 is ERC1155URI {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address public marketAddress;
+
+    string public name;
+    string public symbol;
+
+    constructor(string memory _baseMetadataURI, address _marketAddress)
+        ERC1155(_baseMetadataURI)
+    {
+        marketAddress = _marketAddress;
+        name = "Cruzo";
+        symbol = "CRZ";
+        _setBaseURI(_baseMetadataURI);
+    }
+
+    /**
+     *
+     * @notice This function return totalNumber of unique tokentypes
+     * @dev This will only return the total number of individual unique tokentypes only
+     * @dev It will not return total inluding suppply of fungible tokens
+     *
+     */
+    function total() public view onlyOwner returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    /**
+     *
+     * @notice Internal function to mint to `_amount` of tokens of `_tokenId` to `_to` address
+     * @param _tokenId - The Id of the token to be minted
+     * @param _to - The to address to which the token is to be minted
+     * @param _amount - The amount of tokens to be minted
+     * @dev Can be used to mint any specific tokens
+     *
+     */
+    function _mintToken(
+        uint256 _tokenId,
+        uint256 _amount,
+        address _to,
+        bytes memory _data
+    ) internal returns (uint256) {
+        _mint(_to, _tokenId, _amount, _data);
+        setApprovalForAll(marketAddress, true);
+        return _tokenId;
+    }
+
+    /**
+     *
+     * @notice Internal function to mint to `_amount` of tokens of new tokens to `_to` address
+     * @param _to - The to address to which the token is to be minted
+     * @param _amount - The amount of tokens to be minted
+     * @param _uri - The token metadata uri (optional if tokenURI is set)
+     * @dev Used internally to mint new tokens
+     */
+    function _createToken(
+        uint256 _amount,
+        address _to,
+        string memory _uri,
+        bytes memory _data
+    ) internal returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        creators[newItemId] = _msgSender();
+
+        if (bytes(_uri).length > 0) {
+            _setTokenURI(newItemId, _uri);
+        }
+
+        return _mintToken(newItemId, _amount, _to, _data);
+    }
+
+    /**
+     *
+     * @notice This function can be used to mint a new token to a specific address
+     * @param _to - The to address to which the token is to be minted
+     * @param _amount - The amount of tokens to be minted
+     * @dev Mint a new token  to `to` address
+     *
+     */
+    function create(
+        uint256 _amount,
+        address _to,
+        string memory _uri,
+        bytes memory _data
+    ) public returns (uint256) {
+        return _createToken(_amount, _to, _uri, _data);
+    }
+
+    /**
+     *
+     * @notice Mint a specific token to `_to` address in `_amount` quantiy
+     * @param _tokenId The token ID to be minted
+     * @param _amount - The amount of tokens to be minted
+     * @param _to - The to address to which the token is to be minted
+     * Requirements-
+     *     - Token ID must exist
+     */
+    function mintTo(
+        uint256 _tokenId,
+        uint256 _amount,
+        address _to,
+        bytes memory _data
+    ) public onlyCreator(_tokenId) returns (uint256) {
+        require(
+            _tokenIds.current() >= _tokenId,
+            "token doesn't exist; try using `mintNewTo()`"
+        );
+        return _mintToken(_tokenId, _amount, _to, _data);
+    }
+
+    /**
+     *
+     * @notice SET Uri Type from {DEFAULT,IPFS,ID}
+     * @param _uriType - The uri type selected from {DEFAULT,IPFS,ID}
+     */
+
+    function setURIType(uint256 _uriType) public onlyOwner {
+        _setURIType(_uriType);
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        require(id <= _tokenIds.current(), "Cruzo1155:non existent tokenId");
+        return _tokenURI(id);
+    }
+
+    function setTokenURI(uint256 _id, string memory _uri) public {
+        require(_id <= _tokenIds.current(), "Cruzo1155:non existent tokenId");
+        _setTokenURI(_id, _uri);
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        _setBaseURI(_baseURI);
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,76 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-abi-exporter";
+import { task, HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+dotenv.config();
+const PKS = [process.env.PRIVATE_KEY!];
+const config: HardhatUserConfig = {
+  solidity: "0.8.7",
+  networks: {
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 15000000000,
+      accounts: PKS,
+    },
+    bscMainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: PKS,
+    },
+    polygonMumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: PKS,
+    },
+    polygonMainnet: {
+      url: "https://rpc-mainnet.matic.network",
+      accounts: PKS,
+    },
+    cronosMainnetBeta: {
+      url: "https://evm-cronos.crypto.org",
+      chainId: 25,
+      accounts: PKS,
+    },
+    cronosTestnet: {
+      url: "https://cronos-testnet-3.crypto.org:8545",
+      chainId: 338,
+      accounts: PKS,
+    },
+    avaxMainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: PKS
+    },
+    avaxFuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: PKS
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  abiExporter: {
+    path: './data/abi',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    spacing: 2,
+    pretty: false,
+  }
+};
+
+export default config;
